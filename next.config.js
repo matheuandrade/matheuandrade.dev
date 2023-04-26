778b7154c8ccb4a758bcaca5d1f810f527d53ee0
matheuandrade <matheus.andrade@ntconsultcorp.com>
2023-04-26 11:46:24 -0300

    feat: reorder project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: false,
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
